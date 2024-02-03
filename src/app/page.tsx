52461d1e87990ba6d2074e524a5e9be424e61e40
Farouk <kazeem.farouk@yahoo.com>
2024-02-03 00:14:16 -0500

    added new updates modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,10 +19,9 @@ interface Todo {
   progress?: boolean;
 }
 
-
-
 export default function Home() {
   const getTodo = trpc.getTodo.useQuery();
+  const [modal, setModal] = useState(true);
 
   const { data, isLoading, error } = getTodo;
 
@@ -49,9 +48,6 @@ export default function Home() {
       getTodo.refetch();
     },
   });
-  
-
-
 
   const [input, setInput] = useState("");
 
@@ -75,11 +71,13 @@ export default function Home() {
     updateProgressMutation.mutate({ id, progress });
   };
 
-  function sortObjectsByProgress(inputArray:Todo[]) {
-    return inputArray?.sort((a, b) => (a.progress === b.progress ? 0 : a.progress ? 1 : -1));;
+  function sortObjectsByProgress(inputArray: Todo[]) {
+    return inputArray?.sort((a, b) =>
+      a.progress === b.progress ? 0 : a.progress ? 1 : -1
+    );
   }
-  function filterObjectsByProgress(inputArray:Todo[]) {
-    return inputArray?.filter(obj => obj.progress === true);
+  function filterObjectsByProgress(inputArray: Todo[]) {
+    return inputArray?.filter((obj) => obj.progress === true);
   }
 
   const { isLoaded, userId, sessionId, getToken } = useAuth();
@@ -107,19 +105,21 @@ export default function Home() {
           </div>
 
           <div className="rounded-full w-28 sm:w-16 md:w-24 h-28 sm:h-16 md:h-24 flex items-center justify-center bg-orange-700 text-xl sm:text-lg md:text-4xl font-extrabold text-black">
-            {data?.length! > 0 ? `${filterObjectsByProgress(data!).length}/${data?.length}` : "-"}
+            {data?.length! > 0
+              ? `${filterObjectsByProgress(data!).length}/${data?.length}`
+              : "-"}
           </div>
         </div>
       </div>
-
-      
 
       {/* Input todo list */}
       <InputBar addTodoList={addTodoList} setInput={setInput} input={input} />
 
       <div className="p-3">
-
-      <p className="mt-3 text-sm"> * If u get this pre link write a todo with your name or alias *</p>
+        <p className="mt-3 text-sm">
+          {" "}
+          * If u get this pre link write a todo with your name or alias *
+        </p>
       </div>
 
       {mutation.error && <p>Something went wrong! {mutation.error.message}</p>}
@@ -137,6 +137,64 @@ export default function Home() {
           />
         ))}
       </div>
+
+      {/* Modal */}
+      {modal && (
+        <div
+          id="default-modal"
+          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full text-black"
+        >
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold  dark:text-white">
+                  New Updates
+                </h3>
+                <button
+                  onClick={() => setModal(false)}
+                  type="button"
+                  className="bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="default-modal"
+                >
+                  <svg
+                    className="w-3 h-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              <div className="p-4 md:p-5 space-y-4">
+                <ul>
+                  <li>- Progress button working now</li>
+                  <li>- Sorts completed todo to end</li>
+                  <li>- Optimized DB queries</li>
+                </ul>
+              </div>
+
+              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button
+                  type="button"
+                  className="text-white bg-orange-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={() => setModal(false)}
+                >
+                  Say Less
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
