d1309d2ae39e6752b063983e6393e148d732d631
Farouk <kazeem.farouk@yahoo.com>
2024-02-02 20:25:05 -0500

    fixed vercel production url issue

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -11,7 +11,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: process.env.TRPC_URL!
+          url: `${process.env.VERCEL_URL}/api/trpc`
         }),
       ],
     })
